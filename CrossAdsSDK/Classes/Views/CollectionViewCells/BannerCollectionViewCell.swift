c0c97332adfbc77b4bb182776459b41af6f5fd54
Mohammad Alabed <mfa01@yahoo.com>
2022-11-08 10:22:30 +0300

    fix background color for collections

--- CrossAdsSDK/Classes/Views/CollectionViewCells/BannerCollectionViewCell.swift
+++ CrossAdsSDK/Classes/Views/CollectionViewCells/BannerCollectionViewCell.swift
@@ -31,11 +31,17 @@ public class BannerCollectionViewCell: UICollectionViewCell {
         } else {
             bannerImageView.image = presentation.placeholderImage
         }
-        
+        setBackgroundColor(color: presentation.adModel?.backgroundColor)
         let gesture = UITapGestureRecognizer(target: self, action:  #selector (self.adTapped (_:)))
         contentView.addGestureRecognizer(gesture)
     }
    
+    func setBackgroundColor(color: String?) {
+        if let color = color, let floatColor = Float64(color) {
+            let rgbInt = Int(UInt32(floatColor))
+            contentView.backgroundColor = UIColor(rgb: rgbInt)
+        }
+    }
     
     @objc func adTapped(_ sender:UITapGestureRecognizer){
         // do other task
